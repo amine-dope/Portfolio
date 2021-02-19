c509ffc45309fbd8806ca69e5f06fe1726cf1775
amine-dope <aminesailla85@gmail.com>
2021-02-19 10:55:47 +0100

    finished portfolio

--- port-folio/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navUL = document.getElementById('nav-ul');
-
-hamburger.addEventListener('click', function(){
-    navUL.classList.toggle('show');
-});
